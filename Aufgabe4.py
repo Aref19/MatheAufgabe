41efd7df12cb9b885e3de728fbada1cd11a7094a
Aref <arefobaid6@gmail.com>
2021-10-21 00:25:46 +0200

    aufgabe 4 Fast

--- Aufgabe4.py
+++ Aufgabe4.py
@@ -1,3 +1,8 @@
+from builtins import dict
+
+import Aufgabe3
+
+
 def ToBinear(zahl):
     listm = []
     print(f'le,{listm}')
@@ -22,10 +27,37 @@ def ToBinear(zahl):
     print(f'le,{listm}')
 
 
-print(ToBinear(127))
-key=['','','','','','','','','','','','','']
-def HExToDecimal(zahl):
+#print(ToBinear(127))
+key = dict({"0":0,"1": 1, "2": 2, "3":3, "4": 4, "5":5
+               , "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "A": 10,
+            "B": 11,
+            "C": 12,
+            "D": 13,
+            "E": 14,
+            "F": 15,
+
+            })
 
 
+def HExToDecimal(zahl,true):
+    wert=0
+    list=[]
+    for i in zahl:
+       list.append(i)
+    lengh = len(list)-1
+    for y in list:
+        wert+=key[y]*(16**lengh)
+        print(f'dsa,{lengh}{key[y]}')
+        lengh = lengh - 1
+    print(lengh)
+    if(true):
+        ToBinear(wert)
+
+    print(wert)
 
 
+HExToDecimal("FC2",True)
